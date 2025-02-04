8517b77c284362a106b0c4763521abd99f614df0
ariczhu <aric.zhu@waymarkcare.com>
2025-02-04 16:29:26 -0500

    Replaced cross join with subquery to improve execution time

--- models/claims_preprocessing/encounters/intermediate/encounters__orphaned_claims.sql
+++ models/claims_preprocessing/encounters/intermediate/encounters__orphaned_claims.sql
@@ -15,23 +15,23 @@ with cte as (
   ,stg.start_date
   ,stg.end_date
   ,stg.patient_data_source_id
+  ,(
+    select
+      max(encounter_id)
+    from
+      {{ ref('encounters__combined_claim_line_crosswalk') }}
+  ) as max_encounter_id
   from {{ ref('encounters__stg_medical_claim') }} stg
   left join  {{ ref('encounters__combined_claim_line_crosswalk') }} enc on stg.claim_id = enc.claim_id
-  and
+  and 
   stg.claim_line_number = enc.claim_line_number
   where enc.claim_id is null -- missing from encounter mapping table
-)
-
-,max_encounter as (
-  select max(encounter_id) max_encounter_id
-  from {{ ref('encounters__combined_claim_line_crosswalk') }}
 )
 
 select 
   claim_id
 , claim_line_number
-, dense_rank() over (order by patient_data_source_id, claim_id) + max_encounter.max_encounter_id as encounter_id
+, dense_rank() over (order by patient_data_source_id, claim_id) + max_encounter_id as encounter_id
 , 'orphaned claim' as encounter_type
 , 'other' as encounter_group
 from cte
-cross join max_encounter
